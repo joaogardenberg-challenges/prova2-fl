cb57d681edbcfad6c2ed0639e840e93568a5d467
Joao Gardenberg <joaogardenberg@gmail.com>
2017-06-07 06:10:54 -0300

    API in Ruby done.

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ Feito com: Sublime Text 3 Build 3126
 ## Heroku
 
 Heroku url: **soon...**
+
+## MySQL
+
+**SQL para popular a tabela:**
+
+LOAD DATA LOCAL INFILE 'tickets.csv'
+INTO TABLE tickets
+CHARACTER SET utf8
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
--- tickets_api.rb
+++ tickets_api.rb
@@ -1,13 +1,49 @@
 require 'sinatra'
+require 'mysql2'
+require 'json'
 
 get '/' do
 	redirect '/tickets'
 end
 
 get '/tickets' do
-	$from = params['from']
-	$to = params['to']
-	$order = params['order']
+	$from = params['from'].to_s.downcase
+	$to = params['to'].to_s.downcase
+	$order = params['order'].to_s.downcase
+	$order_type = params['order_type'].to_s.downcase
 
-	erb :index
+	$order_type = "asc" unless $order_type || $order_type != ""
+
+	client = Mysql2::Client.new(
+		host: 'localhost',
+		username: 'root',
+		password: 'linux',
+		database: 'fulllab',
+		encoding: 'utf8'
+	)
+
+	query = "SELECT * FROM `tickets` "
+	
+	query += "WHERE " if ($from && $from != "") || ($to && $to != "")
+	query += "`from` LIKE '%#{$from}%' " if $from && $from != ""
+	query += "AND " if $from && $from != "" && $to && $to != ""
+	query += "`to` LIKE '%#{$to}%' " if $to && $to != ""
+
+	query += "ORDER BY `#{$order}` #{$order_type}" if ($order == "price" || $order == "company" || $order == "departure_date") && ($order_type == "asc" || $order_type == "desc")
+
+	result = client.query(query)
+
+	result_hash = result.map do |row|
+		{
+			price: row['price'],
+			from: row['from'],
+			to: row['to'],
+			image: row['image'],
+			company: row['company'],
+			url: row['url'],
+			departure_date: row['departure_date']
+		}
+	end
+
+	result_hash.to_json
 end
