d2b8fc092fa6d8e60b644867e1351a6aca902756
Joao Gardenberg <joaogardenberg@gmail.com>
2017-06-08 05:35:28 -0300

    CSS styling added.

--- public/index/js/main.js
+++ public/index/js/main.js
@@ -1,4 +1,9 @@
 angular.module("main", ["ngResource"])
+	.filter("reais", function() {
+		return function(input) {
+			return input ? "R$ " + parseFloat(input).toFixed(2) : "";
+		};
+	})
 	.controller("index", ["$scope", "$resource",
 		function($scope, $resource) {
 			var res = $resource("/tickets/");
