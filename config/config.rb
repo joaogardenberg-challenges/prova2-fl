b918d3f5bdbaec6b9d14b32ab4c7770d8cd32a10
Joao Gardenberg <joaogardenberg@gmail.com>
2017-06-08 07:35:59 -0300

    Configuring the database in Heroku.

--- config/config.rb
+++ config/config.rb
@@ -3,8 +3,8 @@
 		adapter: 'mysql2',
 		host: 'localhost',
 		name: 'fulllab',
-		database: ENV['CLEARDB_DATABASE_URL'],
-		user: ENV['CLEARDB_DATABASE_USERNAME'],
-		password: ENV['CLEARDB_DATABASE_PASSWORDs']
+		database: ENV['DATABASE_URL'],
+		user: ENV['DATABASE_USERNAME'],
+		password: ENV['DATABASE_PASSWORDs']
 	}
 }
