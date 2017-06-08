f5acea5e9b83f75b9a1f941173368077db58f1d7
Joao Gardenberg <joaogardenberg@gmail.com>
2017-06-08 07:32:48 -0300

    Configuring the database in Heroku.

--- config/config.rb
+++ config/config.rb
@@ -3,8 +3,8 @@
 		adapter: 'mysql2',
 		host: 'localhost',
 		name: 'fulllab',
-		database: 'fulllab',
-		user: 'root',
-		password: 'linux'
+		database: 'heroku_00b354e8864d5f4',
+		user: 'b63406ed458aee',
+		password: '9478b56d'
 	}
 }
